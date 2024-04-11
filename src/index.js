3067c55f136b0da827c2752487064566f247d19b
kimanziplacide <p.imanzi@alustudent.com>
2024-04-11 11:30:34 +0200

    pushing the static part of the project , still in progress not yet done with the project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+// import './index.css';
+// import App from './App.js';
+import Stars from './Stars.js';
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Stars
+      maxStars={5}
+      messages={['terrible', 'bad', 'okay', 'good', 'amazing']}
+      defaultRating={2}
+    />
+  </React.StrictMode>
+);
